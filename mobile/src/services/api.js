aa782c773d07a0a379b99deba78aac89866615f6
Gabriel Zanluca <gabriel.zanluca@gmail.com>
2019-08-12 00:08:58 -0300

    developed mobile app

---
+++ mobile/src/services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL : 'http://10.0.2.2:3333'
+})
+
+export default api
+
+/** Localhost android:
+ * genymotion : 10.0.3.2:
+ * emludar do android studio : 10.0.2.2: 
+ */
